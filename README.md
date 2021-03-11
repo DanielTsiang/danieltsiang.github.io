524476856415961b86754bfb32c9bcdd7a6a5823
Daniel Tsiang <74436899+DanielTsiang@users.noreply.github.com>
2021-03-11 00:47:09 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# Profile
+
+### Description
+A webpage that displays my CV in an interactive format. Designed with bootstrap to display well on mobile devices.
+
+### Example
+Mobile view:<br>
+![Profile mobile view](https://user-images.githubusercontent.com/74436899/110144425-3b825980-7dd0-11eb-93b1-ea44f6c6be05.png "Profile - Mobile view")
+
+PC view:<br>
+![Profile PC view](https://user-images.githubusercontent.com/74436899/110147349-7c2fa200-7dd3-11eb-818d-9dc4996e8067.png "Profile - PC view")
+
+### Technologies Used
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+* jQuery
+* AJAX
+
+### Goals
+* Create a webpage using HTML, CSS, JavaScript to display my CV in an aesthetic manner.
+* Webpage should have a navigation bar and smooth scrolling.
+* Use bootstrap modals to display my portofolio which can be filtered and popup when clicked on to show more information.
+* Use jQuery and AJAX to create a 'Contact Me' form, a popup should display a 'success' or 'error' message on the same page when user submits form. 
+* Use CSS with bootstrap to style webpage which can displays well on both PC and mobile devices.
+
+### Getting Started
+1. Visit the web page [here](https://daniel-tsiang.netlify.app/).
+
+### Contribution Guidelines
+If you would like to contribute code, identify bugs, or propose improvements, please fork this repository and submit a pull request with your suggestions. Below are some helpful links to help you get started:
+1. [Project's main repository](https://github.com/DanielTsiang/profile)
+2. [Project's issue tracker](https://github.com/DanielTsiang/profile/issues)
+
---
+++ js/custom.js
@@ -0,0 +1,78 @@
+(function($) {
+	"use strict"; // Start of use strict
+
+	// Smooth scrolling using jQuery easing
+	$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+		if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+			var target = $(this.hash);
+			target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+			if (target.length) {
+				$('html, body').animate({
+					scrollTop: (target.offset().top)
+				}, 1000, "easeInOutExpo");
+				return false;
+			}
+		}
+	});
+
+	// Closes responsive menu when a scroll trigger link is clicked
+	$('.js-scroll-trigger').click(function() {
+		$('.navbar-collapse').collapse('hide');
+	});
+
+	// Activate scrollspy to add active class to navbar items on scroll
+	$('body').scrollspy({
+		target: '#sideNav'
+	});
+
+	// Validates the contact form and submits it using Formspree.
+	$.validator.setDefaults({
+		highlight: function(element) {
+			$(element).closest('.form-group').addClass('has-error');
+		},
+		unhighlight: function(element) {
+			$(element).closest('.form-group').removeClass('has-error');
+		},
+		errorElement: 'span',
+		errorClass: 'help-block',
+		errorPlacement: function(error, element) {
+			if(element.parent('.input-group').length) {
+				error.insertAfter(element.parent());
+			} else {
+				error.insertAfter(element);
+			}
+		}
+	});
+
+	$("#contact-form").validate({
+		submitHandler: function(form) {
+			$.ajax({
+				url: "//formspree.io/f/mnqoklgo",
+				method: "POST",
+				data: {
+					name: $(form).find("input[name='name']").val(),
+					_replyto: $(form).find("input[name='_replyto']").val(),
+					_subject: $(form).find("input[name='_subject']").val(),
+					message: $(form).find("textarea[name='message']").val()
+				},
+				dataType: "json",
+				success: function() {
+					$("#submit-success").fadeIn();
+					$("#contact-form").fadeOut();
+				},
+				error: function() {
+					$("#submit-errors").fadeIn();
+				}
+			});
+		}
+	});
+
+	// Hides alert message for form submission error,
+	// allows alert message to reappear if user tries to resubmit form.
+	$(function(){
+		$("[data-hide]").on("click", function() {
+			$(this).closest("." + $(this).attr("data-hide")).hide();
+		});
+	});
+
+})(jQuery); // End of use strict
